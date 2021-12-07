975aa3203940684262d522080497c150a858bc35
Fuminori OKUHARA <f-okuhara@sakura.ad.jp>
2021-12-07 11:44:41 +0900

    WerkerをnRF Connect SDK 1.7.1に対応させる

--- wercker.sh
+++ wercker.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+BOARD=scm-ltem1nrf_nrf9160ns
+
 if [ "$DEBUG" = "1" ]; then
   TARGETS="develop"
 else
@@ -12,13 +14,13 @@ mkdir -p dist/bin
 for TARGET in ${TARGETS}
 do
   echo "${TARGET}"
-    ./build.sh ${TARGET}
+    ./build.sh ${TARGET} ${BOARD}
     RESULT="$?"
     if [ "${RESULT}" -ne 0 ]; then
         echo "Failed"
         exit ${RESULT}
     fi
-    cp -v "build/${TARGET}/zephyr/merged.hex" "dist/bin/${TARGET}-merged.hex"
+    cp -v "build/${BOARD}/${TARGET}/zephyr/merged.hex" "dist/bin/${BOARD}_${TARGET}-merged.hex"
 done
 
 tar zcvf dist/sipf-std-client_nrf9160.tar.gz -C dist bin
